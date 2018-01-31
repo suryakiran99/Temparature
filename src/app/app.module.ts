398a735ef2335ec481222eac1a4ca7fa8a6f7125
suryakiran99 <vasnirkir@gmail.com>
2018-01-31 11:26:24 +0530

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpModule} from '@angular/http';
+import {WeatherService} from './services/weather.service';
 import { AppComponent } from './app.component';
+import { SettingComponent } from './setting/setting.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+
+
+const myRoutes=[
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'setting' , component:SettingComponent
+  },
+  {
+    path:'home',component:HomeComponent
+  }
+
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(myRoutes)
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
